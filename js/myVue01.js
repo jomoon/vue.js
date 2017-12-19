70d357dc7ca66ce6e3ace65df93af03f0072f149
hjj <jo_moon@163.com>
2017-12-19 09:43:45 +0800

    增加了 app5

---
+++ js/myVue01.js
@@ -0,0 +1,54 @@
+window.onload= function() {
+  var app = new Vue({
+    el: "#app1",
+    data: {
+      msg: "俩个Div Id同名会有什么事情呢?",
+      title: "如果有俩个字符呢"
+    }
+
+
+  });
+
+
+
+    var app2 = new Vue({
+      el: "#app2",
+      data: {
+        message: "页面加载于" + new Date().toLocaleString()
+      }
+    });
+
+
+    var app3 = new Vue({
+      el: "#app3",
+      data: {
+        seen : false
+      }
+    });
+
+
+
+    var app4 = new Vue({
+      el: "#app4",
+      data:{
+        todoList:[
+          {text:"学习"} ,
+          {text:"生活"},
+          {text:"娱乐"}
+        ]
+      }
+    });
+
+
+    var app5 = new Vue({
+      el: "#app-5",
+      data: {
+        message: "Hello Vue.js"
+      },
+      methods: {
+        reverseMessage:function() {
+          this.message = this.message.split('').reverse().join('');
+        }
+      }
+    });
+}
